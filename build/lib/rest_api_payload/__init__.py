7e0fd558a39c29ef7f88abafa7838d708aeaad32
Israel Abraham <israelvictory87@gmail.com>
2022-02-28 14:28:23 +0100

    Update version to v0.0.2

---
+++ build/lib/rest_api_payload/__init__.py
@@ -0,0 +1,38 @@
+def error_response(status, message):
+    """
+    Custom error response
+
+    Args:
+        status ([string]): [status message -> 404 not found, 500 server error, 400 bad request, etc]
+        message ([string]): [what error message do you want to show to the end user?]
+
+    Returns:
+        [response]: [Returns a dictionary of status, and message]
+    """
+    
+    payload = {
+        "status": status,
+        "message": message
+    }
+    return payload
+
+
+def success_response(status, message, data):
+    """
+    Custom success response
+
+    Args:
+        status ([string]): [status message -> 201 created, 200 ok, 202 accepted, etc]
+        message ([string]): [what message do you want to show to the end user?]
+        data ([dict]): [serialized data from the server side]
+
+    Returns:
+        [response]: [Returns a dictionary of status, message, data of whatever object that was serialized]
+    """
+    
+    payload = {
+        "status": status,
+        "message": message,
+        "data": data
+    }
+    return payload
