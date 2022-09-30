3dc46f6caac29dca86e741431011c1cc4696ce0c
Abram <israelvictory87@gmail.com>
2022-09-30 16:19:40 +0100

    Bump version to 0.0.6

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ classifiers = [
  
 setup(
   name='rest_api_payload',
-  version='0.0.5',
+  version='0.0.6',
   description='Industry ready custom API payload with an easy format for building Python APIs (Django/Django Rest Framework)',
   long_description_content_type="text/markdown",
   long_description=open('README.md').read(),
   url='https://github.com/israelabraham/API-Payload',  
-  author='Abram 🐼',
+  author='Abram',
   author_email='israelvictory87@gmail.com',
   license='MIT', 
   classifiers=classifiers,
   keywords=['api', 'payload', 'custom api payload'], 
-  packages=find_packages(),
-  install_requires=[''] 
+  packages=find_packages()
 )
