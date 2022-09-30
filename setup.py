703491fbb956e8cafe919f7ca019e4be32451a6d
Abram <israelvictory87@gmail.com>
2022-09-30 16:26:06 +0100

    Minor update: update package description

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 setup(
   name='rest_api_payload',
   version='0.0.6',
-  description='Industry ready custom API payload with an easy format for building Python APIs (Django/Django Rest Framework)',
+  description='A to-go-to production API payload with an easy format for building APIs with Python.',
   long_description_content_type="text/markdown",
   long_description=open('README.md').read(),
   url='https://github.com/israelabraham/API-Payload',  
