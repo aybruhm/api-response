a4756d52258d694855b9dfbb83e6093da8723153
Abram <israelvictory87@gmail.com>
2022-09-30 14:32:19 +0100

    Refactor: update status types and set default value to data

--- rest_api_payload/__init__.py
+++ rest_api_payload/__init__.py
@@ -1,9 +1,9 @@
-def error_response(status:str, message:str):
+def error_response(status:bool, message:str):
     """
-    This function returns a dictionary of status and message
+    Custom error response
     
     :param status: The status code of the response
-    :type status: str
+    :type status: bool
     
     :param message: The message you want to display to the end user
     :type message: str
@@ -18,22 +18,21 @@ def error_response(status:str, message:str):
     return payload
 
 
-def success_response(status:str, message:str, data:dict):
+def success_response(status:bool, message:str, data:dict = {}):
     """
     Custom success response
     
-    :param status: The status code of the response
-    :type status: str
+    :param status: This is a boolean value that indicates whether the request was successful
+    :type status: bool
     
-    :param message: The message you want to show to the end user
+    :param message: This is the message you want to send to the user
     :type message: str
     
-    :param data: dict
+    :param data: This is the data that you want to return to the client
     :type data: dict
     
-    :return: A dictionary of status, message, data of whatever object that was serialized
+    :return: A dictionary with the keys status, message, and data.
     """
-
     
     payload = {
         "status": status,
