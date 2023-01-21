2c90cde142a1992f69a075d7ea5b0e704c2e165f
Abram <israelvictory87@gmail.com>
2023-01-21 15:21:28 +0100

    Update: modify tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -3,38 +3,36 @@ from rest_api_payload import success_response, error_response
 
 
 class TestInit(unittest.TestCase):
-    
     def test_error_response(self):
-        
-        payload = {
-            "status": False,
-            "message": 'BAD REQUEST!'
-        }
+
+        payload = {"status": False, "message": "BAD REQUEST!"}
         self.assertEqual(
-            payload, 
+            payload,
             error_response(
-                status=payload.get("status"), 
-                message=payload.get("message"))
+                status=payload["status"], message=payload["message"]
+            ),
         )
-        
+
     def test_success_response(self):
-        
+
         payload = {
             "status": True,
-            "message": 'OKKK!',
+            "message": "OKKK!",
             "data": {
                 "title": "title 1",
-                "content": 'Some text here',
-                "author": 1
-            }
+                "content": "Some text here",
+                "author": 1,
+            },
         }
         self.assertEqual(
-            payload, 
+            payload,
             success_response(
-                status=payload.get("status"), 
-                message=payload.get("message"),
-                data=payload.get("data"))
+                status=payload["status"],
+                message=payload["message"],
+                data=payload["data"],
+            ),
         )
-        
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     unittest.main()
